3eed2b8843e3d289b16175a0c624a6e4abca5fcb
Alisha <saialisha99@gmail.com>
2020-12-17 15:44:22 +0530

    Initial Commit

---
+++ Provider/AuditSeverityProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AuditSeverity.Models;
+using AuditSeverity.Repository;
+using Newtonsoft.Json;
+namespace AuditSeverity.Provider
+{
+    public class AuditSeverityProvider: IAuditSeverityProvider
+    {
+        private IAuditSeverityRepository _repository;
+        public AuditSeverityProvider(IAuditSeverityRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<AuditResponse> GetAuditType(AuditRequest request)
+        {
+            var outdb = await _repository.GetBenchMarkValues(request);
+            return outdb;
+        }
+    }
+}
---
+++ Repository/AuditSeverityRepository.cs
@@ -0,0 +1,78 @@
+﻿using AuditSeverity.Models;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AuditSeverity.Repository
+{
+    public class AuditSeverityRepository : IAuditSeverityRepository
+    { 
+        Random rand = new Random();
+        public async Task<AuditResponse> GetBenchMarkValues(AuditRequest request)
+        {
+            List<AuditBenchmark> auditList = new List<AuditBenchmark>();
+            using (var client = new HttpClient())
+            {
+                var contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                client.DefaultRequestHeaders.Accept.Add(contentType);
+                using (var response = await client.GetAsync("http://20.62.185.247//api/AuditBenchmark"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    auditList = JsonConvert.DeserializeObject<List<AuditBenchmark>>(apiResponse);
+                }
+                //using (var response = await client.GetAsync("https://localhost:44361/api/AuditBenchmark"))
+                //{
+                //    string apiResponse = await response.Content.ReadAsStringAsync();
+                //    auditList = JsonConvert.DeserializeObject<List<AuditBenchmark>>(apiResponse);
+                //}
+            }
+            string typeofAudit = auditList[0].AuditType;
+            string typeofAudit1 = auditList[1].AuditType;
+            int benchMarkNo = auditList[0].BenchmarkNoAnswers;
+            int benchMarkNo1 = auditList[1].BenchmarkNoAnswers;
+            AuditResponse auditResponse;
+
+            if (request.AuditDetail.AuditType.Equals(typeofAudit) && request.AuditDetail.CountOfNos <= benchMarkNo)
+            {
+                auditResponse = new AuditResponse();
+                auditResponse.AuditId = rand.Next();
+                auditResponse.ProjectExecutionStatus = "GREEN";
+                auditResponse.RemedialActionDuration = "No action needed";
+                return auditResponse;
+            }
+            else if (request.AuditDetail.AuditType.Equals(typeofAudit) && request.AuditDetail.CountOfNos > benchMarkNo)
+            {
+                auditResponse = new AuditResponse();
+                auditResponse.AuditId = rand.Next();
+                auditResponse.ProjectExecutionStatus = "RED";
+                auditResponse.RemedialActionDuration = "Action to be taken in 2 weeks";
+                return auditResponse;
+            }
+            else if (request.AuditDetail.AuditType.Equals(typeofAudit1) && request.AuditDetail.CountOfNos <= benchMarkNo1)
+            {
+                auditResponse = new AuditResponse();
+                auditResponse.AuditId = rand.Next();
+                auditResponse.ProjectExecutionStatus = "GREEN";
+                auditResponse.RemedialActionDuration = "No action needed";
+                return auditResponse;
+            }
+            else if (request.AuditDetail.AuditType.Equals(typeofAudit1) && request.AuditDetail.CountOfNos > benchMarkNo1)
+            {
+                auditResponse = new AuditResponse();
+                auditResponse.AuditId = rand.Next();
+                auditResponse.ProjectExecutionStatus = "RED";
+                auditResponse.RemedialActionDuration = "Action to be taken in 1 week";
+                return auditResponse;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
